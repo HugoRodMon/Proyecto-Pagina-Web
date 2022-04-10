e87c819effd01d70ff97915b1678749798a9bff5
Hugo Rodríguez <45364805+HugoRodMon@users.noreply.github.com>
2022-04-10 22:56:21 +0000

    Primer commit

--- php/login.php
+++ php/login.php
@@ -1,3 +1,11 @@
-<?
+<?php
+//aqui se va a comprobar si los datos estan en la base de datos.
+
+session_start();
+
+$_SESSION['username'] = $_POST['inputUsername'];
+
+echo "Iniciando sesión. Si no te redirige haz clic <a href="../">aquí</a>
+<script>window.location.replace("../");</script>";
 
 ?>
